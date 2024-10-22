445b826066945bd299dcde988fd4266a7653d688
gvrv03 <itsgaurav3112003@gmail.com>
2024-10-22 16:50:37 +0530

    Add Completion form btton

--- src/components/Completion.js
+++ src/components/Completion.js
@@ -1,13 +1,21 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Completion = () => {
   return (
-    <section className='fixed  font-semibold flex gap-5 w-full md:w-auto md:bottom-auto bottom-0 items-center md:top-20 md:right-5 px-5 py-2  rounded-t-md md:rounded-md  bg-blue-500 text-white' >
-        <i className='uil uil-file-info-alt text-2xl' />
-        <Link href="/" >Fill Course Completion Form</Link>
+    <section className="fixed  flex-col flex w-full md:w-auto md:bottom-auto bottom-0 items-center md:top-20 md:right-5 rounded-t-md md:rounded-md  bg-blue-500 text-white">
+      <div className=" px-5 py-2  flex gap-2 items-center" >
+        <i className="uil uil-file-info-alt text-2xl" />
+        <Link href="/">Fill Course Completion Form</Link>
+      </div>
+      <div  className="md:hidden bg-gray-900  font-semibold w-full  text-xs p-2 text-center " >
+        Developed by{" "}
+        <Link href="http://gvrv.in/" className=" text-yellow-300 font-semibold ">
+          gvrv_n
+        </Link>{" "}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Completion
+export default Completion;
