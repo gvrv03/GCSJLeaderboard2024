efd70b70823ee9d98011c1b243dbca93e1638e16
gvrv03 <itsgaurav3112003@gmail.com>
2024-10-22 11:58:48 +0530

    Improve table formatting

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="flex justify-center bg-blue-50  py-5 mt-10 font-semibold">
+      Developed By  <span className=" text-blue-700 ml-2"> Gaurav Narnaware</span>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/MUITable.js
+++ src/components/MUITable.js
@@ -12,13 +12,21 @@ import Button from "@mui/material/Button";
 import SortIcon from "@mui/icons-material/Sort";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Link from "next/link";
 
+import PersonIcon from "@mui/icons-material/Person";
 export default function MUITable({ data }) {
   const [searchTerm, setSearchTerm] = useState("");
   const [sortOrder, setSortOrder] = useState("desc");
   const [sortedColumn, setSortedColumn] = useState("Badges");
 
-  const columnsToExclude = ["Email address", "Timestamp", , "Phone No."];
+  const columnsToExclude = [
+    "Email address",
+    "Timestamp",
+    ,
+    "Phone No",
+    "Email",
+  ];
 
   if (!data || data.length === 0) {
     return (
@@ -28,7 +36,9 @@ export default function MUITable({ data }) {
     );
   }
 
-  const headers = Object.keys(data[0]).filter((header) => !columnsToExclude.includes(header));
+  const headers = Object.keys(data[0]).filter(
+    (header) => !columnsToExclude.includes(header)
+  );
 
   const sortData = (data, column) => {
     return data.sort((a, b) => {
@@ -40,7 +50,9 @@ export default function MUITable({ data }) {
 
   const filteredData = data.filter((row) => {
     return headers.some((header) => {
-      return String(row[header]).toLowerCase().includes(searchTerm.toLowerCase());
+      return String(row[header])
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase());
     });
   });
 
@@ -53,7 +65,7 @@ export default function MUITable({ data }) {
         backgroundColor: "#f9fafb",
         boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
         borderRadius: 3,
-        width: "100%", 
+        width: "100%",
       }}
     >
       <Box sx={{ display: "flex", alignItems: "center", marginBottom: 2 }}>
@@ -63,30 +75,43 @@ export default function MUITable({ data }) {
           fullWidth
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          sx={{ marginBottom: "16px", backgroundColor: "white", borderRadius: 1 }}
+          sx={{
+            marginBottom: "16px",
+            backgroundColor: "white",
+            borderRadius: 1,
+          }}
         />
-        
       </Box>
-      <TableContainer sx={{ maxHeight: 500 }}>
+      <TableContainer sx={{ maxHeight: 600 }}>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
-            <TableRow >
-              <TableCell className=" md:text-xs text-[10px]"   sx={{ fontWeight: "bold", color: "#1976d2", backgroundColor: "#e3f2fd" }}>
-                Sr no.
+            <TableRow>
+              <TableCell
+                className=" md:text-xs text-[10px]"
+                sx={{
+                  fontWeight: "bold",
+                  color: "#1976d2",
+                  backgroundColor: "#e3f2fd",
+                }}
+              >
+                SrNo
               </TableCell>
-              {headers.map((column) => (
-                <TableCell className=" md:text-xs text-[10px]"
-                  key={column}
-                  sx={{
-                    fontWeight: "bold",
-                    backgroundColor: "#e3f2fd",
-                    color: "#1976d2",
-                    minWidth: 100,
-                  }}
-                >
-                  {column}
-                </TableCell>
-              ))}
+              {headers.map((column) => {
+                return (
+                  <TableCell
+                    className=" md:text-xs  text-[10px]"
+                    key={column}
+                    sx={{
+                      fontWeight: "bold",
+                      backgroundColor: "#e3f2fd",
+                      color: "#1976d2",
+                      width:" 100%",
+                    }}
+                  >
+                    {column}
+                  </TableCell>
+                );
+              })}
             </TableRow>
           </TableHead>
           <TableBody>
@@ -99,20 +124,49 @@ export default function MUITable({ data }) {
                   transition: "background-color 0.3s ease",
                 }}
               >
-                <TableCell className=" md:text-xs text-[10px]" >{rowIndex + 1}</TableCell>
+                <TableCell className=" md:text-xs text-[10px]">
+                  {rowIndex + 1}
+                </TableCell>
 
-                {headers.map((header) => (
-                  <TableCell className=" md:text-xs text-[10px]"  key={`${rowIndex}-${header}`}>
-                    {row[header]}
-                  </TableCell>
-                ))}
+                {headers.map((header) => {
+                  return (
+                    <TableCell
+                      className=" md:text-xs text-[10px]"
+                      key={`${rowIndex}-${header}`}
+                    >
+                      {header == "Name" ? (
+                        <div className="flex gap-2 items-center ">
+                          <PersonIcon />
+                          <p>{row[header]}</p>{" "}
+                        </div>
+                      ) : header == "Profile_URL" ? (
+                        <Link
+                          href={row[header]}
+                          u
+                          className=" bg-blue-50 text-center font-semibold text-[8px] p-2 rounded-full "
+                          target="_blank"
+                        >
+                          Profile
+                        </Link>
+                      ) : header != "Name" ? (
+                        <div className="text-center">{row[header]}</div>
+                      ) : (
+                        row[header]
+                      )}
+                    </TableCell>
+                  );
+                })}
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
       {!sortedData.length && (
-        <Typography variant="body1" textAlign="center" sx={{ padding: "16px", color: "#f44336" }}>
+        <Typography
+          variant="body1"
+          textAlign="center"
+          sx={{ padding: "16px", color: "#f44336" }}
+        >
           No data found
         </Typography>
       )}
